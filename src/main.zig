992372ad95fe4e188961763ccc2950e193b39f85
Isaac Spencer <22ispencer@gmail.com>
2025-01-30 16:16:00 -0600

    arraylist for printing

--- src/main.zig
+++ src/main.zig
@@ -4,18 +4,33 @@ const c = @cImport({
 });
 
 pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const alloc = gpa.allocator();
+    defer {
+        const status = gpa.deinit();
+        if (status == .leak) {
+            std.debug.print("Memory leak detected", .{});
+        }
+    }
+
+    _ = c.set_escdelay(60);
     const stdwin = c.initscr();
+    _ = c.noecho();
+    _ = c.cbreak();
+
+    _ = c.keypad(stdwin, true);
     defer _ = c.endwin();
 
     const cols = c.getmaxx(stdwin);
     const lines = c.getmaxy(stdwin);
     const esc_delay = c.get_escdelay();
 
-    var buf = std.mem.zeroes([1024:0]u8);
+    var message = std.ArrayList(u8).init(alloc);
+    defer message.deinit();
 
-    const buf_len = (try std.fmt.bufPrint(&buf, "Escape Delay: {d}", .{esc_delay})).len;
+    try message.writer().print("Esc Delay: {d}\x00", .{esc_delay});
 
-    _ = c.mvprintw(@divTrunc(lines, 2), @divTrunc(cols - @as(c_int, @intCast(buf_len)), 2), &buf);
+    _ = c.mvprintw(@divTrunc(lines, 2), @divTrunc(cols - @as(c_int, @intCast(message.items.len)), 2), message.items.ptr);
 
     _ = c.refresh();
 
