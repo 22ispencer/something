3ac6dbe4f6d7636f2a77a8d95b027148fca14d8a
Isaac Spencer <22ispencer@gmail.com>
2025-01-30 20:11:54 -0600

    backspace working

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,29 @@
 const std = @import("std");
 const c = @cImport({
+    @cInclude("locale.h");
     @cInclude("curses.h");
 });
 
 pub fn cPrint(list: *std.ArrayList(u8), comptime format: []const u8, args: anytype) !void {
     try list.writer().print(format ++ "\x00", args);
+}
+
+pub fn cAppend(list: *std.ArrayList(u8), char: u8) !void {
+    if (list.items.len > 0 and list.getLast() == 0) {
+        _ = list.pop();
+    }
+
+    try list.append(char);
+    try list.append(0);
+}
+
+pub fn cWriteAll(list: *std.ArrayList(u8), str: []const u8) !void {
+    if (list.items.len > 0 and list.getLast() == 0) {
+        _ = list.pop();
+    }
+
+    try list.writer().writeAll(str);
+    try list.append(0);
 }
 
 pub fn main() !void {
@@ -17,6 +36,7 @@ pub fn main() !void {
         }
     }
 
+    _ = c.setlocale(c.LC_ALL, "");
     _ = c.set_escdelay(60);
     const stdwin = c.initscr();
     _ = c.noecho();
@@ -27,16 +47,22 @@ pub fn main() !void {
 
     const cols = c.getmaxx(stdwin);
     const lines = c.getmaxy(stdwin);
-    const esc_delay = c.get_escdelay();
+    // const esc_delay = c.get_escdelay();
 
-    var message = std.ArrayList(u8).init(alloc);
-    defer message.deinit();
+    var buf = std.ArrayList(u8).init(alloc);
+    defer buf.deinit();
 
-    try cPrint(&message, "Esc Delay: {d}", .{esc_delay});
+    while (true) {
+        const char = c.getch();
 
-    _ = c.mvprintw(@divTrunc(lines, 2), @divTrunc(cols - @as(c_int, @intCast(message.items.len)), 2), message.items.ptr);
+        if (char < std.math.maxInt(u8)) {
+            try cAppend(&buf, @intCast(char));
+        } else if (buf.items.len > 1 and char == c.KEY_BACKSPACE) {
+            _ = buf.orderedRemove(buf.items.len - 2);
+            _ = c.clear();
+        }
+        _ = c.mvprintw(@divTrunc(lines, 2), @divTrunc(cols - @as(c_int, @intCast(buf.items.len)), 2), buf.items.ptr);
 
-    _ = c.refresh();
-
-    _ = c.getch();
+        _ = c.refresh();
+    }
 }
