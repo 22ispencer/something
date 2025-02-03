e57e47d592ad339a81c68bfa5500125a82ac6ea3
Isaac Spencer <isaac.spencer@idp.wichita.edu>
2025-02-03 16:11:38 -0600

    no more structs lmao

--- src/main.zig
+++ src/main.zig
@@ -5,35 +5,6 @@ const c = @cImport({
     @cInclude("curses.h");
 });
 
-const Application = struct {
-    const Self = @This();
-    stdwin: ?*c.WINDOW,
-    lines: u16,
-    cols: u16,
-    alloc: std.mem.Allocator,
-    pub fn init(alloc: std.mem.Allocator) Application {
-        _ = c.setlocale(c.LC_ALL, "");
-        _ = c.set_escdelay(60);
-        const stdwin = c.initscr();
-        _ = c.noecho();
-        _ = c.cbreak();
-
-        _ = c.keypad(stdwin, true);
-
-        const lines: u16 = @intCast(c.getmaxy(stdwin));
-        const cols: u16 = @intCast(c.getmaxx(stdwin));
-        return .{
-            .stdwin = stdwin,
-            .lines = lines,
-            .cols = cols,
-            .alloc = alloc,
-        };
-    }
-    pub fn deinit(_: Self) void {
-        _ = c.endwin();
-    }
-};
-
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const alloc = gpa.allocator();
@@ -43,11 +14,25 @@ pub fn main() !void {
             std.debug.print("Memory leak detected", .{});
         }
     }
-    var app = Application.init(alloc);
-    defer app.deinit();
+    _ = c.setlocale(c.LC_ALL, "");
+    _ = c.set_escdelay(60);
+    const stdwin = c.initscr();
+    defer _ = c.endwin();
+    _ = c.noecho();
+    _ = c.cbreak();
+
+    _ = c.keypad(stdwin, true);
+
+    const lines: u16 = @intCast(c.getmaxy(stdwin));
+    const cols: u16 = @intCast(c.getmaxx(stdwin));
 
     var buf = std.ArrayList(u8).init(alloc);
     defer buf.deinit();
 
-    std.time.sleep(std.time.ns_per_s * 3);
+    try buf.writer().print("{d}, {d}\x00", .{ lines, cols });
+
+    _ = c.addstr(buf.items.ptr);
+    _ = c.refresh();
+
+    _ = c.getch();
 }
